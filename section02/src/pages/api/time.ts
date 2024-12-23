07e7edb110faa0d81d373aadca047a4cd1042a83
vgotu99 <vgotu99@gmail.com>
2024-12-23 14:15:32 +0900

    Api routes

---
+++ section02/src/pages/api/time.ts
@@ -0,0 +1,8 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  const date = new Date();
+  res.json({ time: date.toLocaleString() });
+};
+
+export default handler;
